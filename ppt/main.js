eb190154a0964c8d07ef8f875bf78ecd3b80bbd2
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-16 10:00:05 +0200

    Add files via upload

---
+++ ppt/main.js
@@ -0,0 +1,25 @@
+const push = document.getElementById('push');
+const unshift = document.getElementById('unshift');
+const insertAt = document.getElementById('insertAt');
+const addPosition = document.getElementById('addPosition');
+const arrayHtml = document.getElementById('arrayHtml');
+
+const characters = ["🍄", "🥔", "👾", "🪩", "😶‍🌫️", "🤡", "👽", "🐧"];
+const screenArray = [];
+
+const addElement = () => {
+	screenArray.push(randomElement());
+	arrayHtml.innerText = screenArray;
+};
+
+push.addEventListener("click", addElement);
+
+const randomElement = () =>
+	characters[Math.floor(Math.random() * characters.length)];
+	
+/*VAMOSAHI*/
+
+const.deleteElement = () => {
+	screenArray.unshift(deleteElement)
+	arrayHtml.innerText = screenArray;
+}
